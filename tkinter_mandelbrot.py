aacc4dc71d4b8050252a0e5d2c460684e245d3f3
JensDiemer <git@jensdiemer.de>
2011-11-24 20:18:13 +0100

    Put into classes

--- tkinter_mandelbrot.py
+++ tkinter_mandelbrot.py
@@ -1,44 +1,74 @@
+#!/usr/bin/env python
 # coding: utf-8
 
 import time
 import Tkinter as tkinter
 
-WIDTH = 320
-HEIGHT = 240
-LEFT = -2
-RIGHT = 0.5
-TOP = 1.25
-BOTTOM = -1.25
-ITERATIONS = 20
 
-root = tkinter.Tk()
-image = tkinter.PhotoImage(width=WIDTH, height=HEIGHT)
-lb1 = tkinter.Label(root, image=image)
-lb1.pack()
+class Mandelbrot(object):
+    def __init__(self, width=320, height=240, iterations=20, update_time=0.5):
+        self.width = width
+        self.height = height
+        self.iterations = iterations
+        self.update_time = update_time
 
-UPDATE_TIME = 0.2
-start_time = time.time()
-next_update = start_time + UPDATE_TIME
-for y in range(HEIGHT):
-    for x in range(WIDTH):
-        z = complex(0, 0)
-        c = complex(LEFT + x * (RIGHT - LEFT) / WIDTH, TOP + y * (BOTTOM - TOP) / HEIGHT)
-        norm = abs(z) ** 2
-        for count in xrange(ITERATIONS):
-            if norm <= 4.0:
-                z = z * z + c
-                norm = abs(z * z)
-            else:
-                break
+        self.left = -2
+        self.right = 0.5
+        self.top = 1.25
+        self.bottom = -1.25
 
+    def render(self):
+        self.start_time = time.time()
+        next_update = self.start_time + self.update_time
+        for y in xrange(self.height):
+            if time.time() > next_update:
+                now = time.time()
+                next_update = now + self.update_time
+                self.update(now, y)
+
+            for x in xrange(self.width):
+                z = complex(0, 0)
+                c = complex(self.left + x * (self.right - self.left) / self.width, self.top + y * (self.bottom - self.top) / self.height)
+                norm = abs(z) ** 2
+                for count in xrange(self.iterations):
+                    if norm <= 4.0:
+                        z = z * z + c
+                        norm = abs(z * z)
+                    else:
+                        break
+
+                    self.create_pixel(x, y, count, norm)
+
+        self.update(time.time(), self.height)
+
+    def create_pixel(self, x, y, count, norm):
+        raise NotImplemented
+
+    def update(self, y):
+        raise NotImplemented
+
+
+class TkMandelbrot(Mandelbrot):
+    def __init__(self, *args, **kwargs):
+        super(TkMandelbrot, self).__init__(*args, **kwargs)
+        self.root = tkinter.Tk()
+        self.image = tkinter.PhotoImage(width=self.width, height=self.height)
+        lb1 = tkinter.Label(self.root, image=self.image)
+        lb1.pack()
+
+    def create_pixel(self, x, y, count, norm):
         (r, g, b) = (count * 6, 0, 0)
-        image.put("#%02x%02x%02x" % (r, g, b), (x, y))
+        self.image.put("#%02x%02x%02x" % (r, g, b), (x, y))
 
-        if time.time() > next_update:
-            next_update = time.time() + UPDATE_TIME
-            root.update()
+    def update(self, now, y):
+        percent = float(y) / self.height * 100
+        duration = now - self.start_time
+        print "%.1f%% - duration: %.1fsec" % (percent, duration)
+        self.root.update()
 
-total_duration = time.time() - start_time
-print "duration: %.1fsec, %.1f pixel/sec" % (total_duration, HEIGHT * WIDTH / total_duration)
 
-root.mainloop()
+if __name__ == '__main__':
+    m = TkMandelbrot()
+    m.render()
+    m.root.mainloop()
+
